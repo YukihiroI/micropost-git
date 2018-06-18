7d44e466784d863a22ae0f4946e93f8cd56090e7
yukihiro.iwata <yukihiro.iwata@rakuten.com>
2018-06-18 06:05:50 +0000

    Procfile

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -18,14 +18,17 @@ class MicropostsController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
             ];
+            $data += $this->counts($user);
+            return view('users.show', $data);
+        }else {
+            return view('welcome');
         }
-        return view('welcome', $data);
     }
         public function store(Request $request)
     {
